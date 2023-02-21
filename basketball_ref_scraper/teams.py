ee9e80817c095c8f2f0dfa32204893a91f8a3bc3
Jonathan <qinjonat@usc.edu>
2023-02-20 20:08:45 -0800

    custom basketball ref scraper

---
+++ basketball_ref_scraper/teams.py
@@ -0,0 +1,216 @@
+import pandas as pd
+from requests import get
+from bs4 import BeautifulSoup
+
+try:
+    from constants import TEAM_TO_TEAM_ABBR, TEAM_SETS
+    from utils import remove_accents
+except:
+    from basketball_ref_scraper.constants import TEAM_TO_TEAM_ABBR, TEAM_SETS
+    from basketball_ref_scraper.utils import remove_accents
+
+# edited get_roster function to resolve null player names
+def get_roster(team, season_end_year):
+    r = get(
+        f'https://www.basketball-reference.com/teams/{team}/{season_end_year}.html')
+    df = None
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        table = soup.find('table')
+        df = pd.read_html(str(table))[0]
+        df.columns = ['NUMBER', 'PLAYER', 'POS', 'HEIGHT', 'WEIGHT', 'BIRTH_DATE',
+                        'NATIONALITY', 'EXPERIENCE', 'COLLEGE']
+        df['BIRTH_DATE'] = df['BIRTH_DATE'].apply(
+            lambda x: pd.to_datetime(x) if pd.notna(x) else pd.NaT)
+        df['NATIONALITY'] = df['NATIONALITY'].apply(
+            lambda x: x.upper() if pd.notna(x) else '')
+    return df
+    
+# original web-scraper code commented out
+
+# def get_roster_original(team, season_end_year):
+#     r = get(
+#         f'https://www.basketball-reference.com/teams/{team}/{season_end_year}.html')
+#     df = None
+#     if r.status_code == 200:
+#         soup = BeautifulSoup(r.content, 'html.parser')
+#         table = soup.find('table')
+#         df = pd.read_html(str(table))[0]
+#         df.columns = ['NUMBER', 'PLAYER', 'POS', 'HEIGHT', 'WEIGHT', 'BIRTH_DATE',
+#                       'NATIONALITY', 'EXPERIENCE', 'COLLEGE']
+#         # remove rows with no player name (this was the issue above)
+#         df = df[df['PLAYER'].notna()]
+#         df['PLAYER'] = df['PLAYER'].apply(
+#             lambda name: remove_accents(name, team, season_end_year))
+#         # handle rows with empty fields but with a player name.
+#         df['BIRTH_DATE'] = df['BIRTH_DATE'].apply(
+#             lambda x: pd.to_datetime(x) if pd.notna(x) else pd.NaT)
+#         df['NATIONALITY'] = df['NATIONALITY'].apply(
+#             lambda x: x.upper() if pd.notna(x) else '')
+
+#     return df
+
+
+# def get_team_stats(team, season_end_year, data_format='PER_GAME'):
+#     if data_format == 'TOTAL':
+#         selector = 'div_totals-team'
+#     elif data_format == 'PER_GAME':
+#         selector = 'div_per_game-team'
+#     elif data_format == 'PER_POSS':
+#         selector = 'div_per_poss-team'
+#     r = get(
+#         f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{season_end_year}.html&div={selector}')
+#     df = None
+#     if r.status_code == 200:
+#         soup = BeautifulSoup(r.content, 'html.parser')
+#         table = soup.find('table')
+#         df = pd.read_html(str(table))[0]
+#         league_avg_index = df[df['Team'] == 'League Average'].index[0]
+#         df = df[:league_avg_index]
+#         df['Team'] = df['Team'].apply(lambda x: x.replace('*', '').upper())
+#         df['TEAM'] = df['Team'].apply(lambda x: TEAM_TO_TEAM_ABBR[x])
+#         df = df.drop(['Rk', 'Team'], axis=1)
+#         df.loc[:, 'SEASON'] = f'{season_end_year-1}-{str(season_end_year)[2:]}'
+#         s = df[df['TEAM'] == team]
+#         return pd.Series(index=list(s.columns), data=s.values.tolist()[0])
+
+
+# def get_opp_stats(team, season_end_year, data_format='PER_GAME'):
+#     if data_format == 'TOTAL':
+#         selector = 'div_totals-opponent'
+#     elif data_format == 'PER_GAME':
+#         selector = 'div_per_game-opponent'
+#     elif data_format == 'PER_POSS':
+#         selector = 'div_per_poss-opponent'
+#     r = get(
+#         f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{season_end_year}.html&div={selector}')
+#     df = None
+#     if r.status_code == 200:
+#         soup = BeautifulSoup(r.content, 'html.parser')
+#         table = soup.find('table')
+#         df = pd.read_html(str(table))[0]
+#         league_avg_index = df[df['Team'] == 'League Average'].index[0]
+#         df = df[:league_avg_index]
+#         df['Team'] = df['Team'].apply(lambda x: x.replace('*', '').upper())
+#         df['TEAM'] = df['Team'].apply(lambda x: TEAM_TO_TEAM_ABBR[x])
+#         df = df.drop(['Rk', 'Team'], axis=1)
+#         df.columns = list(map(lambda x: 'OPP_'+x, list(df.columns)))
+#         df.rename(columns={'OPP_TEAM': 'TEAM'}, inplace=True)
+#         df.loc[:, 'SEASON'] = f'{season_end_year-1}-{str(season_end_year)[2:]}'
+#         s = df[df['TEAM'] == team]
+#         return pd.Series(index=list(s.columns), data=s.values.tolist()[0])
+
+
+# def get_team_misc(team, season_end_year):
+#     r = get(
+#         f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{season_end_year}.html&div=div_advanced-team')
+#     df = None
+#     if r.status_code == 200:
+#         soup = BeautifulSoup(r.content, 'html.parser')
+#         table = soup.find('table')
+#         df = pd.read_html(str(table))[0]
+#         df.columns = list(map(lambda x: x[1], list(df.columns)))
+#         league_avg_index = df[df['Team'] == 'League Average'].index[0]
+#         df = df[:league_avg_index]
+#         df['Team'] = df['Team'].apply(lambda x: x.replace('*', '').upper())
+#         df['TEAM'] = df['Team'].apply(lambda x: TEAM_TO_TEAM_ABBR[x])
+#         df = df.drop(['Rk', 'Team'], axis=1)
+#         df.rename(columns={'Age': 'AGE', 'Pace': 'PACE', 'Arena': 'ARENA',
+#                   'Attend.': 'ATTENDANCE', 'Attend./G': 'ATTENDANCE/G'}, inplace=True)
+#         df.loc[:, 'SEASON'] = f'{season_end_year-1}-{str(season_end_year)[2:]}'
+#         s = df[df['TEAM'] == team]
+#         s = s.loc[:, ~s.columns.str.contains('^Unnamed')]
+#         return pd.Series(index=list(s.columns), data=s.values.tolist()[0])
+
+
+# def get_roster_stats(team: list, season_end_year: int, data_format='PER_GAME', playoffs=False):
+#     if playoffs:
+#         period = 'playoffs'
+#     else:
+#         period = 'leagues'
+#     selector = data_format.lower()
+#     r = get(
+#         f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2F{period}%2FNBA_{season_end_year}_{selector}.html&div=div_{selector}_stats')
+#     df = None
+#     possible_teams = [team]
+#     for s in TEAM_SETS:
+#         if team in s:
+#             possible_teams = s
+#     if r.status_code == 200:
+#         soup = BeautifulSoup(r.content, 'html.parser')
+#         table = soup.find('table')
+#         df2 = pd.read_html(str(table))[0]
+#         for index, row in df2.iterrows():
+#             if row['Tm'] in possible_teams:
+#                 if df is None:
+#                     df = pd.DataFrame(columns=list(row.index)+['SEASON'])
+#                 row['SEASON'] = f'{season_end_year-1}-{str(season_end_year)[2:]}'
+#                 df = df.append(row)
+#         df.rename(columns={'Player': 'PLAYER', 'Age': 'AGE',
+#                   'Tm': 'TEAM', 'Pos': 'POS'}, inplace=True)
+#         df['PLAYER'] = df['PLAYER'].apply(
+#             lambda name: remove_accents(name, team, season_end_year))
+#         df = df.reset_index().drop(['Rk', 'index'], axis=1)
+#         return df
+
+# def get_team_ratings(*, team=[], season_end_year: int):
+
+#     # Scrape data from URL
+#     r = get(f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{season_end_year}_ratings.html&div=div_ratings')
+#     if r.status_code == 200:
+#         soup = BeautifulSoup(r.content, 'html.parser')
+#         table = soup.find('table')
+#         df = pd.read_html(str(table))[0]
+
+#         # Clean columns and indexes
+#         df = df.droplevel(level=0, axis=1)
+        
+#         df.drop(columns=['Rk', 'Conf', 'Div', 'W', 'L', 'W/L%'], inplace=True)
+#         upper_cols = list(pd.Series(df.columns).apply(lambda x: x.upper()))
+#         df.columns = upper_cols
+
+#         df['TEAM'] = df['TEAM'].apply(lambda x: x.upper())
+#         df['TEAM'] = df['TEAM'].apply(lambda x: TEAM_TO_TEAM_ABBR[x])
+
+#         # Add 'Season' column in and change order of columns
+#         df['SEASON'] = f'{season_end_year-1}-{str(season_end_year)[2:]}'
+#         cols = df.columns.tolist()
+#         cols = cols[0:1] + cols[-1:] + cols[1:-1]
+#         df = df[cols]
+
+#         # Add the ability to either pass no teams (empty list), one team (str), or multiple teams (list)
+#         if len(team) > 0:
+#             if isinstance(team, str):
+#                 list_team = []
+#                 list_team.append(team)
+#                 df = df[df['TEAM'].isin(list_team)]
+#             else:
+#                 df = df[df['TEAM'].isin(team)]
+                    
+#     return 
+
+print("bad")
+team = TEAM_TO_TEAM_ABBR["ATLANTA HAWKS"]
+year = 2022
+r = get(
+        f'https://www.basketball-reference.com/leagues/NBA_2022.html')
+r = get(
+        f'https://www.basketball-reference.com/teams/{team}/{year}.html')
+df = None
+if r.status_code == 200:
+    soup = BeautifulSoup(r.content, 'html.parser')
+    table = soup.find('table')
+    tables = soup.find_all('table')
+    df = pd.read_html(str(tables))[2]
+    print(df)
+
+    # league_avg_index = df[df['Team'] == 'League Average'].index[0]
+    # df = df[:league_avg_index]
+    # df['Team'] = df['Team'].apply(lambda x: x.replace('*', '').upper())
+    # df['TEAM'] = df['Team'].apply(lambda x: TEAM_TO_TEAM_ABBR[x])
+    # df = df.drop(['Rk', 'Team'], axis=1)
+    # df.columns = list(map(lambda x: 'OPP_'+x, list(df.columns)))
+    # df.rename(columns={'OPP_TEAM': 'TEAM'}, inplace=True)
+    # df.loc[:, 'SEASON'] = f'{season_end_year-1}-{str(season_end_year)[2:]}'
+    # s = df[df['TEAM'] == team]
+    # res = pd.Series(index=list(s.columns), data=s.values.tolist()[0])
---
+++ basketball_ref_scraper/utils.py
@@ -0,0 +1,121 @@
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+import unicodedata
+# import unidecode
+
+
+def get_game_suffix(date, team1, team2):
+    r = get(f'https://www.basketball-reference.com/boxscores/index.fcgi?year={date.year}&month={date.month}&day={date.day}')
+    suffix = None
+    if r.status_code==200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        for table in soup.find_all('table', attrs={'class': 'teams'}):
+            for anchor in table.find_all('a'):
+                if 'boxscores' in anchor.attrs['href']:
+                    if team1 in anchor.attrs['href'] or team2 in anchor.attrs['href']:
+                        suffix = anchor.attrs['href']
+    return suffix
+
+"""
+    Helper function for inplace creation of suffixes--necessary in order
+    to fetch rookies and other players who aren't in the /players
+    catalogue. Added functionality so that players with abbreviated names
+    can still have a suffix created.
+"""
+def create_last_name_part_of_suffix(potential_last_names):
+    last_names = ''.join(potential_last_names)
+    if len(last_names) <= 5:
+        return last_names[:].lower()
+    else:
+        return last_names[:5].lower()
+
+"""
+    Amended version of the original suffix function--it now creates all
+    suffixes in place.
+    Since basketball reference standardizes URL codes, it is much more efficient
+    to create them locally and compare names to the page results. The maximum
+    amount of times a player code repeats is 5, but only 2 players have this
+    problem--meaning most player URLs are correctly accessed within 1 to 2
+    iterations of the while loop below.
+    Added unidecode to make normalizing incoming string characters more
+    consistent.
+    This implementation dropped player lookup fail count from 306 to 35 to 0.
+"""
+def get_player_suffix(name):
+    normalized_name = unidecode.unidecode(unicodedata.normalize('NFD', name).encode('ascii', 'ignore').decode("utf-8"))
+    if normalized_name == 'Metta World Peace' :
+        suffix = '/players/a/artesro01.html'
+    else:
+        split_normalized_name = normalized_name.split(' ')
+        if len(split_normalized_name) < 2:
+            return None
+        initial = normalized_name.split(' ')[1][0].lower()
+        all_names = name.split(' ')
+        first_name_part = unidecode.unidecode(all_names[0][:2].lower())
+        first_name = all_names[0]
+        other_names = all_names[1:]
+        other_names_search = other_names
+        last_name_part = create_last_name_part_of_suffix(other_names)
+        suffix = '/players/'+initial+'/'+last_name_part+first_name_part+'01.html'
+    player_r = get(f'https://www.basketball-reference.com{suffix}')
+    while player_r.status_code == 404:
+        other_names_search.pop(0)
+        last_name_part = create_last_name_part_of_suffix(other_names_search)
+        initial = last_name_part[0].lower()
+        suffix = '/players/'+initial+'/'+last_name_part+first_name_part+'01.html'
+        player_r = get(f'https://www.basketball-reference.com{suffix}')
+    while player_r.status_code==200:
+        player_soup = BeautifulSoup(player_r.content, 'html.parser')
+        h1 = player_soup.find('h1')
+        if h1:
+            page_name = h1.find('span').text
+            """
+                Test if the URL we constructed matches the 
+                name of the player on that page; if it does,
+                return suffix, if not add 1 to the numbering
+                and recheck.
+            """
+            if ((unidecode.unidecode(page_name)).lower() == normalized_name.lower()):
+                return suffix
+            else:
+                page_names = unidecode.unidecode(page_name).lower().split(' ')
+                page_first_name = page_names[0]
+                if first_name.lower() == page_first_name.lower():
+                    return suffix
+                # if players have same first two letters of last name then just
+                # increment suffix
+                elif first_name.lower()[:2] == page_first_name.lower()[:2]:
+                    player_number = int(''.join(c for c in suffix if c.isdigit())) + 1
+                    if player_number < 10:
+                        player_number = f"0{str(player_number)}"
+                    suffix = f"/players/{initial}/{last_name_part}{first_name_part}{player_number}.html"
+                else:
+                    other_names_search.pop(0)
+                    last_name_part = create_last_name_part_of_suffix(other_names_search)
+                    initial = last_name_part[0].lower()
+                    suffix = '/players/'+initial+'/'+last_name_part+first_name_part+'01.html'
+
+                player_r = get(f'https://www.basketball-reference.com{suffix}')
+
+    return None
+
+
+def remove_accents(name, team, season_end_year):
+    alphabet = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZY ')
+    if len(set(name).difference(alphabet))==0:
+        return name
+    r = get(f'https://www.basketball-reference.com/teams/{team}/{season_end_year}.html')
+    team_df = None
+    best_match = name
+    if r.status_code==200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        table = soup.find('table')
+        team_df = pd.read_html(str(table))[0]
+        max_matches = 0
+        for p in team_df['Player']:
+            matches = sum(l1 == l2 for l1, l2 in zip(p, name))
+            if matches>max_matches:
+                max_matches = matches
+                best_match = p
+    return 
