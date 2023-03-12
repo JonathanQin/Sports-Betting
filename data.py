1f8d8c6f47111b6c64f3514e5f1e18bd37703857
Jonathan <qinjonat@usc.edu>
2023-03-11 19:04:41 -0800

    deleted:    data.py

--- data.py
+++
@@ -1,2 +0,0 @@
-# using https://github.com/vishaalagartha/basketball_reference_scraper
-# widget service discontinued: https://www.sports-reference.com/blog/2022/10/sports-reference-will-turn-off-our-widgets-service-january-1-2023/
